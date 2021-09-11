62e6c3560115c4476d224dc6320713844e7c2ddf
Nafis Arinda Rizky Putra Handoko <69886565+NafisHandoko@users.noreply.github.com>
2021-09-11 05:22:47 +0000

    add slide feature on script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert(1)
+var slideNow = 0;
+displaySlide(slideNow);
+function move(n){
+  slideNow+=n
+  displaySlide(slideNow)
+}
+function displaySlide(slideNum){
+  var slide = document.querySelectorAll('.slide');
+  if(slideNum>slide.length-1){slideNow=0}
+  if(slideNum<0){slideNow=slide.length-1}
+  for(i=0;i<slide.length;i++){
+    slide[i].style.display = 'none';
+  }
+  slide[slideNow].style.display = 'block';
+}
